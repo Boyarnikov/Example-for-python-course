a38d01e20d859da44b1b9a06407e6a5f32a80465
Boyar <iboyarnikov@gmail.com>
2023-12-04 20:20:37 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Hello world")
+x = 10
+print("Hello world2")
+x = 20
+print("Hello world3")
+
+
+def f(x):
+    return x + 3
+
+
+class H:
+    def __init__(self):
+        pass
+
+    x = 5
+    y = 10
