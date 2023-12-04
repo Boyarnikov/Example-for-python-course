fd92e2d62bb97f118c2ac4efa32ec525d1657f5b
Boyar <iboyarnikov@gmail.com>
2023-12-04 20:40:24 +0300

    Add module.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import module
+
 print("Hello world")
 x = 10
 print("Hello world2")
 x = 20
 print("Hello world3")
 
+print(module.data)
 
 def f(x):
     return x + 3
---
+++ module.py
@@ -0,0 +1,5 @@
+print("module added")
+
+data = {"first": 1, "second": 2}
+
+print("module finished")
